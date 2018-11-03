d94bcdf08a2b5f94efebe24e52d5b59a1ef28455
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-11-03 20:26:20 +0100

    Add clc where its necessary

--- zad1_Wrzesien2018/main.asm
+++ zad1_Wrzesien2018/main.asm
@@ -14,7 +14,7 @@ _main PROC
 	; eax <- trzymanie kolejnych zakodowanych podwojnych slow z bufora
 	; ebx <- trymanie kolejnych bitów zdekodowanych
 	mov esi, offset bufor ; aby esi wskazywalo na najmlodszy bit bufora
-	mov edi, offset bufor + 64 ; do edi przypisuje adres poczatkowy wynikow
+	mov edi, offset bufor + 2048 ; do edi przypisuje adres poczatkowy wynikow
 ptl:
 	mov eax, [esi]
 	mov ecx, 31 ; ilosc przesuniec ecx do dekodowania poszczegolnych bitow
@@ -30,26 +30,32 @@ sprawdz_kolejna_1:
 	clc	; czyszcze CF aby sprawdzic najstarszy bit zakodowany bit w eax
 	bt eax,31 ; bo eax bylo przesuniete juz. Sprawdzam czy po przesunieciu przedostatni bit takze jest 1
 	jnc wynik_dekodowania_0
+	clc
 	bts ebx, 0	; w ebx tymczasowy wynik dekodowania
 	shl ebx, 1 ; aby dekodowac kolejne bity ebx przesuwamy w lewo o bit aby na to miejsce zapisac kolejny zdekodowany
 	clc	;trzeba wyzerowac aby w dalszym obiegu petli CF nie kolidowalo
 	jmp ptl_przesuniec
 wynik_dekodowania_0:
-	btr ebx, 0
+	btr ebx, 0	; zapisanie do CF 0 a na najmlodszym bicie 0
+	clc
 	shl ebx, 1
 	jmp ptl_przesuniec
 sprawdz_kolejno_0: ; wchodzi tutaj jesli CF=0. Sprawdzam czy najstarszy bit takze jest 0
 	clc
 	bt eax, 31
 	jc wynik_dekodowania_0 ; odwrotnie niz w sprawdz_kolejna_1 bo wynik dekodowania to zanegowany XOR dwóch ostatnich bitow
+	clc
 	bts ebx,0
 	shl ebx, 1
 	clc
 	jmp wynik_dekodowania_0
 zapisz_wynik_do_pamieci:
+	mov [edi], ebx	; zapisanie do pamieci wyniku rejestr ebx ktory wystepuje jako tymczasowa zmienna do dekodowania sygnalu
+	mov eax, 0	; aby nie powodowalo zaklaman w eax i ebx w kolejnych obiegach petli
+	mov ebx, 0
 	add edi, 4 ; zwiekszenie adresu bufora o 4 poniewaz operuje na podwojnych slowach (4 bajtach)
 	add esi, 4 ; zwiekszenie adresu wynikow dekodowania o 4 poniewaz operuje na podwojnych slowach (4 bajtach)
-	cmp esi, offset bufor + 64
+	cmp esi, offset bufor + 2048
 	jnz ptl
 	nop
 	call _ExitProcess@4
